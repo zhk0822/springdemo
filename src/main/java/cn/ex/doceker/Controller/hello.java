23881724d5498183f555b895a6b1e04b8e465c73
周慧凯 <zhouhk_0822@163.com>
2020-06-15 11:43:58 +0800

    第2次提交 springboot

--- src/main/java/cn/ex/doceker/Controller/hello.java
+++ src/main/java/cn/ex/doceker/Controller/hello.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class hello {
     @RequestMapping("Hello")
     public String hello(){
+        System.out.print("!111");
         return "hello spring";
+
     }
 }
